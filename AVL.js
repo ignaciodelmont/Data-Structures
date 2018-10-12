878f0860d82f237ad600003e61afcedd9aac0d68
Ignacio Delmont <ignaciodelmont@gmail.com>
2018-10-12 10:03:31 -0300

    added nodes per level count

--- AVL.js
+++ AVL.js
@@ -6,11 +6,12 @@ let Node = function() {
   this.right;
   this.height;
   this.balance;
+  this.level;
 
   //  Insertion
   this.insert = function(value) {
     let arr = [];   // this array will contain the path from root to the node to be inserted
-    node = this;
+    let node = this;
 
     while (node) {  // iterates over the tree while it's not empty
       if (node.value == undefined) {  // finding new node to assign value
@@ -42,6 +43,7 @@ let Node = function() {
 
   // update values and balance the tree
   function update(arr) {
+    setLevels(arr[0]);
     for (i = arr.length - 1; i >= 0; i--) { // sets height and balances for every element in the array obtained in the "insert" function
       setHeight(arr[i]);
       setBalance(arr[i]);
@@ -274,8 +276,55 @@ let Node = function() {
           stack.push({node: x.node.right, state: 0});
       }
     }
-
   }
+
+  this.nodesPerLevel = function() {
+    let node = this;
+    setLevels(node);
+    let levels = [];
+    let stack = [];
+    stack.push(node);
+    let i = -1;
+    while (stack.length > 0) {
+      x = stack.pop();
+      if (x.level > i) {
+        i++;
+        levels.push({level: i, nodes: 1});
+      } else if (x.level == i) {
+        levels[i].nodes++;
+      }
+
+
+      if (x.left)
+        stack.unshift(x.left);
+      if (x.right)
+        stack.unshift(x.right);
+    }
+
+    for (i=0; i < levels.length; i++) {
+      levels[i].occupied = '%'+(levels[i].nodes / (2**i) * 100).toString();
+    }
+    return levels;
+  }
+
+  function setLevels(node) {
+    let stack = [];
+    stack.push(node);
+    node.level = 0;
+    while (stack.length > 0) {
+      let x = stack.pop();
+
+      if (x.right){
+        x.right.level = x.level + 1;
+        stack.push(x.right);
+      }
+      if (x.left){
+        x.left.level = x.level + 1;
+        stack.push(x.left);
+      }
+    }
+  }
+
 }
 
 
@@ -283,13 +332,12 @@ let Node = function() {
 
 let tree = new Node();
 
-for (j=0; j < 10; j++) {
-  let aux = Math.floor(Math.random() * 100000);
+for (j=0; j < 16383; j++) {
+  let aux = (Math.random() * 10000);
   tree = tree.insert(aux);
 }
+
+let levels = tree.nodesPerLevel();
+console.log(levels);
 let data = JSON.stringify(tree, null, 2);
 fs.writeFileSync('avlTree.json', data);
-
-tree.preorder();
-tree.inorder();
-tree.postorder();
